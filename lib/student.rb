7475a548dcc68f4ed54773c003f52d091f468459
Riccardo <riccardo_london@live.it>
2019-03-17 10:20:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,9 @@
 require_relative './user.rb'
 
 class Student < User
-  attr_accessor :knowledge
+  
   @@knowledge = []
 
-  def initialize(knowledge = nil)
-    @@knowledge << knowledge
-  end
 
 def learn(argument)
   @@knowledge << argument
